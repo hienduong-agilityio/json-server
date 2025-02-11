01c6d2deab2246615e43415553d04a6bf8ed2d5f
HienDuong <hien.duong@asnet.com.vn>
2025-02-11 09:24:05 +0700

    fix: update bug api

--- api/server.js
+++ api/server.js
@@ -1,14 +1,16 @@
 const jsonServer = require("json-server");
 const jsonServerAuth = require("json-server-auth");
+const express = require("express");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+server.use(express.json());
 server.use(middlewares);
 
 const rules = jsonServerAuth.rewriter({
-  // Permission rules
   users: 600,
   products: 644,
   carts: 600,
@@ -19,7 +21,6 @@ server.use(rules);
 server.use(jsonServerAuth);
 server.use(router);
 
-server.listen(port);
-
-// Export the Server API
-module.exports = server;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
